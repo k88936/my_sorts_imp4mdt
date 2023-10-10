1e04d740eb344b8ed3f33236c1bab224e56f135c
A4-Tacks <wdsjxhno1001@163.com>
2023-10-11 00:40:55 +0800

    add insert_sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,112 @@
 //! 这是我用来写排序算法示例程序的
+
+/// 提供同种类型的`lt`比较函数,
+/// 将其扩展到`<` `<=` `>` `>=`
+///
+/// 这可以以更可读的方式使用`lt`
+#[macro_export]
+macro_rules! cmp {
+    ($cmp:expr => $a:expr, (<), $b:expr $(,)?) => {
+        $cmp($a, $b)
+    };
+    ($cmp:expr => $a:expr, (<=), $b:expr $(,)?) => {
+        ! $cmp($b, $a)
+    };
+    ($cmp:expr => $a:expr, (>), $b:expr $(,)?) => {
+        $cmp($b, $a)
+    };
+    ($cmp:expr => $a:expr, (>=), $b:expr $(,)?) => {
+        ! $cmp($a, $b)
+    };
+}
+
+pub mod sorts;
+
+#[cfg(test)]
+mod tests {
+    use rand::seq::SliceRandom;
+    use super::data::Data;
+    use crate::sorts::{
+        insert_sort::*,
+    };
+
+    #[test]
+    fn ptr_cmp_test() {
+        let arr = [2, 1];
+        let [a, b]: [*const i32; 2] = [&arr[0], &arr[1]];
+        assert!(a < b);
+        assert_eq!(a, a);
+        assert_eq!(b, b);
+
+        assert!(! (a >= b));
+
+        let arr = Box::new([2, 1]);
+        let [a, b]: [*const i32; 2] = [&arr[0], &arr[1]];
+        assert!(a < b);
+        assert_eq!(a, a);
+        assert_eq!(b, b);
+
+        assert!(! (a >= b));
+    }
+
+    #[test]
+    fn cmp_macro_test() {
+        let lt = i32::lt;
+
+        assert!(cmp!(lt => &1, (<), &2));
+        assert!(cmp!(lt => &1, (<=), &2));
+        assert!(cmp!(lt => &1, (<=), &1));
+        assert!(cmp!(lt => &2, (>), &1));
+        assert!(cmp!(lt => &2, (>=), &1));
+        assert!(cmp!(lt => &1, (>=), &1));
+
+        assert!(! cmp!(lt => &1, (<), &1));
+        assert!(! cmp!(lt => &1, (<=), &0));
+        assert!(! cmp!(lt => &1, (>), &1));
+        assert!(! cmp!(lt => &0, (>=), &1));
+    }
+
+    #[test]
+    fn insert_sort_example_test() {
+        let mut arr = Data::new_vec(1000);
+        arr.shuffle(&mut rand::thread_rng());
+        let mut arr1 = arr.clone();
+        arr.sort();
+        insert_sort_by_lt_example(&mut arr1, Data::lt);
+        assert_eq!(arr, arr1);
+    }
+
+    #[test]
+    fn insert_sort_test() {
+        let mut arr = Data::new_vec(1000);
+        arr.shuffle(&mut rand::thread_rng());
+        let mut arr1 = arr.clone();
+        arr.sort();
+        insert_sort(&mut arr1);
+        assert_eq!(arr, arr1);
+    }
+}
+#[cfg(test)]
+pub(crate) mod data {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
+    pub(crate) struct Data {
+        num: i32,
+        id: i32,
+    }
+    impl Data {
+        pub fn new(num: i32, id: i32) -> Self { Self { num, id } }
+        pub fn new_vec(count: usize) -> Vec<Self> {
+            let mut id = 0;
+            Vec::from_iter((0..count).map(|n| {
+                let res = Self::new((n >> 3) as i32, id);
+                id += 1;
+                res
+            }))
+        }
+    }
+    impl PartialOrd for Data {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            self.num.partial_cmp(&other.num)
+        }
+    }
+}
---
+++ src/sorts/insert_sort.rs
@@ -0,0 +1,77 @@
+use std::{
+    mem,
+    ptr::copy_nonoverlapping as copy_nonol, cmp::Ordering,
+};
+
+/// 清晰的实现, 但是只是清晰
+#[allow(dead_code)]
+pub(crate) fn insert_sort_by_lt_example<T, F>(array: &mut [T], mut lt: F)
+where F: FnMut(&T, &T) -> bool,
+      T: Copy
+{
+    #[warn(dead_code)]
+    for i in 1..array.len() {
+        if cmp!(lt => &array[i-1], (<=), &array[i]) {
+            // 已经排好
+            continue
+        }
+        let target = array[i];
+        let mut j = i - 1;
+        array[i] = array[j];
+        while j > 0 && cmp!(lt => &array[j-1], (>), &target) {
+            array[j] = array[j-1];
+            j -= 1
+        }
+        array[j] = target
+    }
+}
+
+fn insert_sort_by_lt<T, F>(array: &mut [T], mut lt: F)
+where F: FnMut(&T, &T) -> bool
+{
+    for i in 1..array.len() { unsafe {
+        let target_ref = array.get_unchecked(i);
+        if cmp!(lt => array.get_unchecked(i-1), (<=), target_ref) {
+            continue
+        }
+        let mut target = mem::MaybeUninit::uninit();
+        copy_nonol(target_ref, target.as_mut_ptr(), 1);
+        let dst = array.get_unchecked_mut(i) as *mut T;
+        let mut insert_ptr = dst.sub(1);
+        copy_nonol(
+            insert_ptr,
+            dst,
+            1,
+        );
+        while insert_ptr > array.as_mut_ptr()
+            && cmp!(lt =>
+                insert_ptr.sub(1).as_ref().unwrap_unchecked(),
+                (>),
+                target.as_ptr().as_ref().unwrap_unchecked(),
+            )
+        {
+            copy_nonol(insert_ptr.sub(1), insert_ptr, 1);
+            insert_ptr = insert_ptr.sub(1)
+        }
+        copy_nonol(target.as_ptr(), insert_ptr, 1)
+    } }
+}
+
+pub fn insert_sort<T: Ord>(array: &mut [T]) {
+    insert_sort_by_lt(array, T::lt)
+}
+
+pub fn insert_sort_by<T, F>(array: &mut [T], mut cmp: F)
+where F: FnMut(&T, &T) -> Ordering
+{
+    insert_sort_by_lt(array, |a, b| cmp(a, b).is_lt())
+}
+
+pub fn insert_sort_by_key<T, K, F>(array: &mut [T], mut f: F)
+where F: FnMut(&T) -> K,
+      K: Ord,
+{
+    insert_sort_by_lt(array, |a, b| f(a).lt(&f(b)))
+}
+
+
